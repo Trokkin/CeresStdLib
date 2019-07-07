e51c668bac68c86f01f8bdc46d0f31ce6cf8f78a
Trokkin <trokkin@gmail.com>
2019-07-07 23:51:28 +0300

    Fixed Log

--- base/Log.lua
+++ base/Log.lua
@@ -1,4 +1,5 @@
 require('CeresStdLib.container.List')
+require('CeresStdLib.base.Init')
 
 LogLevel = {
 	TRACE = 0,
@@ -7,63 +8,44 @@ LogLevel = {
 	WARNING = 3,
 	ERROR = 4
 }
+LogLevel.Tags = {
+	[LogLevel.TRACE] = '|cffADADADtrace|r - ',
+	[LogLevel.DEBUG] = '|cff2685DCdebug|r - ',
+	[LogLevel.INFO] = '|cffFFCC00info|r - ',
+	[LogLevel.WARNING] = '|cffF47E3Ewarning|r - ',
+	[LogLevel.ERROR] = '|cffFB2700error|r - '
+}
+
 
 DEBUG_LEVEL = -1 -- LogLevel.INFO
 DEBUG_MSG_DURATION = 45
 
 function LogLevel.getTag(level)
-	if level < 2 then
-		if level < 1 then
-			return '|cffADADADtrace|r'
-		else
-			return '|cff2685DCdebug|r'
-		end
+	local r = LogLevel.Tags[level]
+	if r ~= nil then
+		return r
 	end
-	if level > 2 then
-		if level < 4 then
-			return '|cffF47E3Ewarning|r'
-		else
-			return '|cffFB2700error|r'
-		end
-	end
-	return '|cffFFCC00info|r'
+	return ''
 end
 
 Log = {}
----@type List
-Log.storage = List:new()
 
-ceres.addHook("main::after", function()
-	for i = Log.storage.first, Log.storage.last do
-		p = Log.storage[i]
-		if DEBUG_LEVEL <= p.level then
-			DisplayTimedTextToPlayer(
-				GetLocalPlayer(),
-				0.,
-				0.,
-				DEBUG_MSG_DURATION,
-				LogLevel.getTag(p.level) .. ' - ' .. p.msg
-			)
-		end
+local arr = {}
+init(function()
+	for i, p in pairs(arr) do
+		DisplayTimedTextToPlayer(GetLocalPlayer(), 0., 0., DEBUG_MSG_DURATION,
+			LogLevel.getTag(p.level) .. ' - ' .. p.msg)
 	end
-	Log.storage = nil
+	arr = nil
 end)
 
 function printLog(level, msg)
-	if Log.storage ~= nil then
-		local q = { level = level, msg = msg }
-		
-		List.pushright(Log.storage, q)
-		-- Log.storage:pushright(q) -- TODO: fix metatable?
-	else
-		if DEBUG_LEVEL <= level then
-			DisplayTimedTextToPlayer(
-				GetLocalPlayer(),
-				0.,
-				0.,
-				DEBUG_MSG_DURATION,
-				LogLevel.getTag(level) .. ' - ' .. msg
-			)
+	if DEBUG_LEVEL <= level then
+		if arr ~= nil then
+			table.insert(arr, {level = level, msg = msg})
+		else
+			DisplayTimedTextToPlayer(GetLocalPlayer(), 0., 0., DEBUG_MSG_DURATION,
+				LogLevel.getTag(level) .. ' - ' .. msg)
 		end
 	end
 end
