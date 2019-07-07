ab4d6e9b4da940c0c237defa1f079aaf9a19686e
Trokkin <trokkin@gmail.com>
2019-07-07 23:55:08 +0300

    Fixed Optimizations

--- base/Optimizations.lua
+++ base/Optimizations.lua
@@ -3,26 +3,24 @@ require('CeresStdLib.base.Execute')
 require('CeresStdLib.base.Init')
 
 Unoptimized = {}
-localplayer = nil
 players = {}
 
-localplayer = GetLocalPlayer()
 for i = 0, bj_MAX_PLAYER_SLOTS - 1 do
 	players[i] = Player(i)
 end
+localplayer = nil
+
 
 local function replaceNative(name, new_f)
-	if not Unoptimized[name] then
+	if not Unoptimized[name] and _G[name] then
 		Unoptimized[name] = _G[name]
 	end
-	_G[name] = nil
+	_G[name] = new_f
 end
 
---[[ -- TODO: fix
-replaceNative("Player", function(i)
-	return players[i]
+replaceNative("Player", function(i) return players[i] end)
+
+ceres.addHook("main::before", function()
+	localplayer = GetLocalPlayer()
+	replaceNative("GetLocalPlayer", function() return localplayer end)
 end)
-replaceNative("GetLocalPlayer", function()
-	return localplayer
-end)
-]]
