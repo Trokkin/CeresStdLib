49551220ed22586838adcbd1fc0e93737cc87d11
Trokkin <trokkin@gmail.com>
2019-07-11 16:07:32 +0300

    Simplified Rawcode module.

--- base/Rawcode.lua
+++ base/Rawcode.lua
@@ -1,21 +1,19 @@
-CHARMAP =
-	".................................!.#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[.]^_`abcdefghijklmnopqrstuvwxyz{|}~................................................................................................................................."
-
 function toRawCode(int)
-	res = ''
+	local res = ''
 	for i = 1, 4 do
-		res = CHARMAP[math.floor(int / 256)] .. res
+		res = string.char(math.floor(int / 256)) .. res
 		int = int % 256
 	end
 	return res
 end
 
 function fromRawCode(raw)
-	res = 0
-	f = 1
+	return FourCC(raw)
+	--[[local res = 0
+	local f = 1
 	for i = 1, 4 do
-		res = res + f * (CHARMAP:find(raw:sub(i, i), nil, true) - 1)
+		res = res + f * raw:sub(i, i):byte()
 		f = f * 256
 	end
-	return res
+	return res]]
 end
