7c88384581d1e34e8c06e6d00805a5c29087a0b0
Trokkin <trokkin@gmail.com>
2019-07-16 20:22:00 +0300

    added warning for Native if replacing nil

--- base/Native.lua
+++ base/Native.lua
@@ -1,7 +1,12 @@
+require('CeresStdLib.base.Log')
+
 --- It is generally not recommended to use old natives.
 Native = {}
 
 function replaceNative(name, new_f)
+	if not _G[name] then
+		Log.warn('replaceNative: ' .. name .. ' is not a native.')
+	end
 	if not Native[name] and _G[name] then
 		Native[name] = _G[name]
 	end
