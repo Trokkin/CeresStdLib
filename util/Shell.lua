0219e4cc05cfa66e9f1f8fed4e17fcff704fa4b8
Trokkin <trokkin@gmail.com>
2019-07-16 22:55:41 +0300

    Modernized Shell

--- util/Shell.lua
+++ util/Shell.lua
@@ -1,3 +1,5 @@
+require('CeresStdLib.base.Log')
+
 -- Enables player(0) to turn chat into Lua shell.
 -- Run anything you want.
 
@@ -5,13 +7,17 @@ local shell = CreateTrigger()
 TriggerRegisterPlayerChatEvent(shell, Player(0), '%', false)
 TriggerAddAction(shell, function()
     local s = GetEventPlayerChatString()
-    s = SubString(s, 1, StringLength(s))
+    if s:sub(1,1) ~= '%' then
+        return
+    end
+    s = s:sub(2)
     local f = load(s)
     if not f then
-        print('invalid shell command \'' .. s .. '\'')
+        Log.error('invalid shell command "' .. s .. '"')
     end
     f()
 end)
+
 
 function KillShell()
 	if shell ~= nil then
