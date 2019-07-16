27196ebf2ee03d45219e21616be4f48bc55d6b19
Trokkin <trokkin@gmail.com>
2019-07-16 20:20:43 +0300

    Extended Execute with assert and execute with custom function handler

--- base/Execute.lua
+++ base/Execute.lua
@@ -1,12 +1,26 @@
 require('CeresStdLib.base.Log')
+require('CeresStdLib.base.Native')
 
---- Safely executes the function, and in case of an error, prints it in Log and returns `nil`.
+--- Safely executes the function, and in case of an error, call `handler(msg)` and returns `nil`.
 --- Else, returns everything what was returned by the function.
-function execute(f, ...)
+---@param f function
+---@param handler function
+function xexecute(f, handler, ...)
 	r = {pcall(f, ...)}
 	if not r[1] then
-		Log.error(r[2])
+		handler(table.unpack(r, 2))
 		return nil
 	end
 	return table.unpack(r, 2)
 end
+
+--- Safely executes the function, and in case of an error, prints it in Log and returns `nil`.
+--- Else, returns everything what was returned by the function.
+---@param f function
+function execute(f, ...)
+	xexecute(f, function(msg) Log.error(msg) end, ...)
+end
+
+replaceNative('assert', function(...)
+	execute(Native.assert, ...)
+end)
