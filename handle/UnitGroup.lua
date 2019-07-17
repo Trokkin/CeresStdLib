a587f900416022836b4902a2814e88e53fe28c0d
MediumRareSteak-Hive <jsncastillo712@gmail.com>
2019-07-17 23:21:51 +0800

    Update UnitGroup.lua

--- handle/UnitGroup.lua
+++ handle/UnitGroup.lua
@@ -1,17 +1,17 @@
 require('CeresStdLib.base.Native')
 
-local __u = u
+UnitGroup = {}
 
-replaceNative('GetEnumUnit', function() return __u end)
+replaceNative('GetEnumUnit', function() return UnitGroup.u end)
 replaceNative('ForGroup', function(g, func)
     local i = 0
     local j = BlzGroupGetSize(g)
-    local u = __u
+    local u = UnitGroup.u
 
     while (i < j) do
-        __u     = BlzGroupUnitAt(g, i)
+        UnitGroup.u = BlzGroupUnitAt(g, i)
         func()
-        i       = i + 1
+        i           = i + 1
     end
-    __u     = u    
+    UnitGroup.u = u    
 end)
