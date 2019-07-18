b9310db415ad23c115bfc572f811d40f8d3d857e
MediumRareSteak-Hive <jsncastillo712@gmail.com>
2019-07-18 10:51:13 +0800

    Update Basics.lua
    Added a new (optional) parameter in function makeReadonly (for debug purposes).

--- base/Basics.lua
+++ base/Basics.lua
@@ -21,20 +21,27 @@ DUMMY_HOSTILE_PLAYER = players[PLAYER_NEUTRAL_AGGRESSIVE]
 local META_TABLE    = {
     TABLES          = {},
     METATABLES      = {},
-    __newindex      = function(t, k, v) Log.warn('Attempted to assign value', v, ' to key ', k, ' in readonly table.') end,
+    NAME            = {},
     __metatable     = false
 }
-META_TABLE.__index  = function(t, k)
+META_TABLE.__index      = function(t, k)
     return META_TABLE.METATABLES[t][k]
 end
+META_TABLE.__newindex   = function(t, k, v)
+    Log.warn('Attempted to assign value ' .. tostring(v) .. ' to key ' .. k .. ' in readonly table ' .. (META_TABLE.NAME[t] or '') .. '.') 
+end
 
-function makeReadonly(table)
+function makeReadonly(table, name)
     -- local lastMetatable     = getmetatable(table)    
     --  If the table is already readonly, return the proxy table
     if META_TABLE.TABLES[table] then return META_TABLE.TABLES[table] end
+    
     local proxy                     = {}
     META_TABLE.METATABLES[proxy]    = table
     META_TABLE.TABLES[table]        = proxy
+
+    if name then META_TABLE.NAME[proxy] = name end
+    
     setmetatable(proxy, META_TABLE)
     return proxy
 end
