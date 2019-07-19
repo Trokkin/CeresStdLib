1f83979481b719723bbfe96c9d3a539723541db6
MediumRareSteak-Hive <jsncastillo712@gmail.com>
2019-07-19 17:07:12 +0800

    Update timer.lua
    Fixed Timer.__index and Timer.__newindex

--- handle/timer.lua
+++ handle/timer.lua
@@ -4,51 +4,49 @@ Timer			= {
 	__handles	= {},
 	__props		= {
 		id			= {
-				get	= function(t) return GetHandleId(t.__obj) end
+				get			= function(t) return GetHandleId(t.__obj) end,
 		},
 		remaining	= {
-				get = function(t) return TimerGetRemaining(t.__obj) end
+				get 		= function(t) return TimerGetRemaining(t.__obj) end
 		},
 		timeout		= {
-				get = function(t) return TimerGetTimeout(t.__obj) end
+				get 		= function(t) return TimerGetTimeout(t.__obj) end
 		},
 		elapsed		= {
-				get = function(t) return TimerGetElapsed(t.__obj) end
+				get 		= function(t) return TimerGetElapsed(t.__obj) end
 		},
 		handle		= {
-				get = function(t) return t.__obj end
+				get 		= function(t) return t.__obj end
 		}
 	}
 }
 
 function Timer.__index(t, k)
-	if Timer.__props[k].get then
+	if Timer.__props[k] then
 		return Timer.__props[k].get(t)
 	end
-	return t[k]
+	return rawget(Timer, k)
 end
 
 function Timer.__newindex(t, k, v)
-	if Timer.__props[k].set then
+	if Timer.__props[k] then
 		Timer.__props[k].set(t, v)
 	end
-	rawset(t, k, v)
 end
 
 function Timer.wrap(t)
 	local i = GetHandleId(t)
 	if not Timer.__handles[i] then
-		Timer.__handles[i] = {obj = t}
+		Timer.__handles[i] = {__obj = t}
 		setmetatable(Timer.__handles[i], Timer)
 	end
 	return Timer.__handles[i]
 end
 
-function Timer:unwrap()	Timer.__handles[self.id] = nil end
-
-function Timer.create()	return Timer.wrap(CreateTimer()) end
+function Timer.create() return Timer.wrap(CreateTimer()) end
 function Timer.getExpired() return Timer.wrap(GetExpiredTimer()) end
 
+function Timer:unwrap()	Timer.__handles[self.id] = nil end
 function Timer:resume()	ResumeTimer(self.__obj) end
 function Timer:start(dur, looped, func)	TimerStart(self.__obj, dur, looped, func) end
 function Timer:pause() PauseTimer(self.__obj) end
