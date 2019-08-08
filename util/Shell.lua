360dadc7fc2aa0d9e36345ba6530f082ebdf53ab
Trokkin <trokkin@gmail.com>
2019-08-08 06:33:29 +0300

    Updated shell with file IO, enabling kind of hotcode reload

--- util/Shell.lua
+++ util/Shell.lua
@@ -1,27 +1,61 @@
 require('CeresStdLib.base.Log')
+require('CeresStdLib.util.file')
 
 -- Enables player(0) to turn chat into Lua shell.
 -- Run anything you want.
 
+local cache = {}
+
 local shell = CreateTrigger()
-TriggerRegisterPlayerChatEvent(shell, Player(0), '%', false)
+TriggerRegisterPlayerChatEvent(shell, Player(0), '$', false)
 TriggerAddAction(shell, function()
+    local s = GetEventPlayerChatString()
+    if s:sub(1,1) ~= '$' then
+        return
+    end
+    s = s:sub(2)
+    ExecuteString(s)
+end)
+
+function ExecuteString(s)
+    local f = load(s)
+    if not f then
+        Log.error('invalid shell command "' .. s .. '"')
+    end
+    print(f())
+    table.insert(cache, s)
+end
+
+local buffer = {''}
+local chatbuffer = CreateTrigger()
+TriggerRegisterPlayerChatEvent(chatbuffer, Player(0), '%', false)
+TriggerAddAction(chatbuffer, function()
     local s = GetEventPlayerChatString()
     if s:sub(1,1) ~= '%' then
         return
     end
     s = s:sub(2)
-    local f = load(s)
-    if not f then
-        Log.error('invalid shell command "' .. s .. '"')
-    end
-    f()
+    table.insert(buffer, s)
 end)
 
+function ExecuteBuffer()
+    ExecuteString(table.concat(buffer, '\n'))
+    buffer = {''}
+end
 
 function KillShell()
 	if shell ~= nil then
 		DestroyTrigger(shell)
 		shell = nil
 	end
+end
+
+function SaveShell()
+    ofstream.open("LuaShellDump.pld")
+    ofstream.write(table.concat(cache, '\n'))
+    ofstream.close()
+end
+
+function LoadShell()
+    ExecuteString(loadfile("LuaShellDump.pld"))
 end
