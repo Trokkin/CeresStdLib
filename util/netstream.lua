f5ac3265b93de5a5a2d498598d03942545729009
Trokkin <trokkin@gmail.com>
2019-09-10 19:54:06 +0300

    Updated netstream

--- util/netstream.lua
+++ util/netstream.lua
@@ -1,72 +1,86 @@
 netstream = {}
-netstream.channels = {}
+local netstream = netstream
 
-function netstream.onRecieve(name, f)
-	if netstream.channels[name] == nil then
-		local ch = {}
-		netstream.channels[name] = ch
-		ch.listeners = {}
-		ch.recieved = {}
-		ch.queue = {}
-		ch.listener = CreateTrigger()
-		for i, p in pairs(players) do
-			BlzTriggerRegisterPlayerSyncEvent(ch.listener, p, name, true)
-			BlzTriggerRegisterPlayerSyncEvent(ch.listener, p, name, false)
-		end
-		TriggerAddAction(ch.listener, function ()
-			local s = BlzGetTriggerSyncData()
-			local p = GetTriggerPlayer()
-		    if s:sub(1,2) == '^{' then
-				ch.recieved[p] = {}
-		        s = s:sub(3)
-		    end
-		    if ch.recieved[p] == nil then
-		        Log.warn('netstream', name, 'headless package!')
-		        return
-		    end
-		    if s:sub(-2) == '^}' then
-		        table.insert(ch.recieved[p],s:sub(1, -3))
-		        s = table.concat(ch.recieved[p])
-				ch.recieved[p] = nil
-				for i, f_ in ipairs(ch.listeners) do
-					f_(s, p)
-				end
-		    else
-		        table.insert(ch.recieved[p], s)
-		    end
-		end)
+local function createChannel(name)
+	local ch = {}
+	netstream.channel = ch
+	ch.name = name
+	ch.listeners = {}
+	ch.recieved = {}
+	ch.queue = {}
+	ch.listener = CreateTrigger()
+	for i, p in pairs(players) do
+		BlzTriggerRegisterPlayerSyncEvent(ch.listener, p, name, true)
+		BlzTriggerRegisterPlayerSyncEvent(ch.listener, p, name, false)
 	end
-	table.insert(netstream.channels[name].listeners, f)
-end
-
-function netstream.send(name, s)
-	local ch = netstream.channels[name]
-	if ch == nil then return end
-	table.insert(ch.queue, '^{'..s..'^}')
-	if ch.timer == nil then
-		local i = 1
-		local n = 1
-		local c = 0
-		ch.timer = CreateTimer()
-		TimerStart(ch.timer, 1/32., true, function()
-			while #ch.queue > 0 do
-				local s_ = ch.queue[1]
-				while i < #s_ do
-					-- Assuming it returns false before desync
-					Log.info(s_:sub(i,i+254))
-					if not BlzSendSyncData(name, s_:sub(i,i+254)) then
-						n = n + 1
-						return
-					end
-					c = c + 1
-					i = i + 255
-				end
-				i = 1
-				table.remove(ch.queue, 1)
+	TriggerAddAction(ch.listener, function ()
+		local s = BlzGetTriggerSyncData()
+		local p = GetTriggerPlayer()
+	    if s:sub(1,2) == '^{' then
+			ch.recieved[p] = {}
+	        s = s:sub(3)
+	    end
+	    if ch.recieved[p] == nil then
+	        Log.warn('netstream', name, 'headless package!')
+	        return
+	    end
+	    if s:sub(-2) == '^}' then
+	        table.insert(ch.recieved[p],s:sub(1, -3))
+	        s = table.concat(ch.recieved[p])
+			ch.recieved[p] = nil
+			for i, f_ in ipairs(ch.listeners) do
+				f_(s, p)
 			end
-			Log.info('net.send took', n, 'seconds', c, 'packages')
-			DestroyTimer(ch.timer)
-			ch.timer = nil
-		end)
+	    else
+	        table.insert(ch.recieved[p], s)
+	    end
+	end)
+end
+
+local function createChannelTimer()
+	local i = 1
+	local n = 1
+	local c = 0
+	local ch = netstream.channel
+	ch.timer = CreateTimer()
+	TimerStart(ch.timer, 1/32., true, function()
+		while #ch.queue > 0 do
+			local s_ = ch.queue[1]
+			while i < #s_ do
+				-- TODO: Check if it returns false before desync to notice that no more data should be synced
+				if not BlzSendSyncData(ch.name, s_:sub(i,i+254)) then
+					n = n + 1
+					return
+				end
+				c = c + 1
+				i =
+					 i + 255
+			end
+			i = 1
+			table.remove(ch.queue, 1)
+		end
+		DestroyTimer(ch.timer)
+		ch.timer = nil
+	end)
+end
+
+--- Adds listener that will trigger when a string would be synchronized.
+--- Listener is called in fashion `f(string, sourcePlayer)`
+---@param f function
+function netstream.onRecieve(f)
+	if netstream.channel == nil then
+		createChannel()
+	end
+	table.insert(netstream.channel.listeners, f)
+end
+
+--- Syncs a string to all players (including self).
+---@param s string
+function netstream.send(s)
+	table.insert(netstream.channel.queue, '^{'..s..'^}')
+	if netstream.channel.timer == nil then
+		createChannelTimer()
 	end
 end
+
+return netstream
